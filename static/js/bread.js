324d92b7aa30b19f77fd14b55876fb4d9e1c26b6
Jesse Fenton <gwaDyckuL1@yahoo.com>
2025-08-07 18:37:57 -0600

    refactor done

--- static/js/bread.js
+++ static/js/bread.js
@@ -8,13 +8,32 @@ document.addEventListener("DOMContentLoaded", function() {
         const breadOptions = document.querySelectorAll(".bread-options")
         breadOptions.forEach(option => {
             option.style.display = 'none';
+            document.getElementById('bread-submission').style.display = 'none'
         })
 
+        const ingredients = document.querySelectorAll(".ingredient")
+        ingredients.forEach(ingredient => {
+            ingredient.style.display = 'none';
+            document.getElementById('tangzhong-choice').value = 'No'
+        })
+        if (selection != '' ){
+            document.getElementById('bread-submission').style.display = 'block'
+        }
         if (selection === 'flour-weight') {
-            document.getElementById('flour-weight').style.display = 'block';
+            document.getElementById('flour-weight').style.display = 'flex';
+            document.getElementById('flour-view').style.display = 'flex';
+            document.getElementById('hydration-view').style.display = 'flex';
+            document.getElementById('fat-view').style.display = 'flex';
+            document.getElementById('sugar-view').style.display = 'flex';
+
         }
         if (selection === 'total-weight') {
-            document.getElementById('total-weight').style.display = 'block';
+            document.getElementById('total-weight').style.display = 'flex';
+            document.getElementById('dough-weight-view').style.display = 'flex';
+            document.getElementById('hydration-view').style.display = 'flex';
+            document.getElementById('fat-view').style.display = 'flex';
+            document.getElementById('sugar-view').style.display = 'flex';
+            
         }
         if (selection === 'pan-dimension') {
             document.getElementById('pan-dimension').style.display = 'block';
@@ -30,14 +49,16 @@ document.addEventListener("DOMContentLoaded", function() {
                     document.getElementById('round').style.display = 'block'
                 }
             })
+            document.getElementById('hydration-view').style.display = 'flex';
+            document.getElementById('fat-view').style.display = 'flex';
+            document.getElementById('sugar-view').style.display = 'flex';
         }
         if (selection != '') {
             document.getElementById('tangzhong-view').style.display = 'block';
         }
 
         tangzhongChoice.addEventListener('change', function() {
-            const tangzhonSelect = this.value
-
+            const tangzhonSelect = this.value;
             if (tangzhonSelect === 'Yes') {
                 document.getElementById('tangzhong-select').style.display = 'block';
             }
