dbd8c52ed5cd258c44d5880aeb582295ae130d7c
Jesse Fenton <gwaDyckuL1@yahoo.com>
2025-09-18 07:16:29 -0700

    added new table to database and modified users table

--- database/init.go
+++ database/init.go
@@ -13,17 +13,30 @@ func OpenDatabase() *sql.DB {
 		log.Fatal("Failed to open database. Error:", err)
 	}
 
-	createTableSQL := `
-	CREATE TABLE IF NOT EXISTS users (
-		username TEXT NOT NULL UNIQUE,
-		name TEXT,
-		email TEXT NOT NULL,
-		password TEXT NOT NULL
-	);
-	`
-	_, err = db.Exec(createTableSQL)
-	if err != nil {
-		log.Fatal("Error creating user table:", err)
+	createTableSQL := []string{
+		`CREATE TABLE IF NOT EXISTS users (
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			username TEXT NOT NULL UNIQUE,
+			name TEXT,
+			email TEXT NOT NULL,
+			password TEXT NOT NULL,
+			create_date DATETIME DEFAULT CURRENT_TIMESTAMP
+		);`,
+		`CREATE TABLE IF NOT EXISTS recipe (
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			user_id INTEGER NOT NULL,
+			recipe_name TEXT NOT NULL,
+			recipe_data TEXT NOT NULL,
+			create_date DATETIME DEFAULT CURRENT_TIMESTAMP,
+			FOREIGN KEY (user_id) REFERENCES users(id)
+		)`,
+	}
+
+	for _, table := range createTableSQL {
+		_, err = db.Exec(table)
+		if err != nil {
+			log.Fatal("Error creating user table:", err)
+		}
 	}
 	return db
 }
